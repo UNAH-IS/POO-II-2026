850041cc427ad58f0a6b8b0526c1324b32029154
Erick Marin <erick.marin.v@gmail.com>
2026-05-26 17:45:54 -0600

    Cambio local en hola mundo

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,5 @@
 public class HolaMundo {
 	public static void main(String args[]) {
-		System.out.println("Hola Mundo, hice un cambio desde github");
+		System.out.println("Hola Mundo, hice un cambio desde mi compu");
 	}
 }
